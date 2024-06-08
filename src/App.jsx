28f5bdf26ecf7e8cb8bb3ed944e280ee2b637a1f
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-08 14:24:06 +0530

    Created App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
-    <div>
-      
+    <div className='main'>
+      <div id="left"></div>
+      <div id="right"></div>
     </div>
   )
 }
