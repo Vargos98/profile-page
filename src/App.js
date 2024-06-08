4e0179139eccfbd1276110495a64373f874c865f
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-08 21:07:59 +0530

    Updated new files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='main'>
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
+  )
+}
+
+export default App
